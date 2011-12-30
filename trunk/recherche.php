6ed806b0bb4224de546336d76edf42f03f97c714
adrien.rochedy@gmail.com <adrien.rochedy@gmail.com@f1f51ce7-b3c9-5cac-4b3b-980ca275825e>
2011-12-30 15:51:00 +0000

    Ajout des focntions de recherche de base ! (marche avec criteres d'age !)

---
+++ trunk/recherche.php
@@ -0,0 +1,56 @@
+ <?php
+ 
+ 
+ session_start();
+// on vérifie toujours qu'il s'agit d'un membre qui est connecté
+if (!isset($_SESSION['login'])) {
+	// si ce n'est pas le cas, on le redirige vers l'accueil
+	header ('Location: index.php');
+	exit();
+}
+
+
+	   include_once('fonctions.php');
+	  $affichage = new Fonctions;
+	  $affichage->menu(FALSE);
+	  include_once('fonctions.php');
+	  $connexion = new Fonctions;
+	  $connexion->connexion();
+		 
+	echo'
+<form action="resultatr.php" method="post">
+
+
+Nom<input type="checkbox" name="joueur[]" value="nom " />
+<input type="text" name="nom" id="pseudo" tabindex="20"/><br />
+
+ Age<input type="checkbox" name="joueur[]" value="age " />
+<input type="text" name="agemin" id="pseudo" tabindex="2"/>
+<input type="text" name="agemax" id="pseudo" tabindex="2"/><br />
+
+Talent <input type="checkbox" name="joueur[]" value="talent " /><br />
+Attaque  <input type="checkbox" name="joueur[]" value="attaque" /><br />
+<input type="submit" />
+
+
+</form>';
+	
+   
+ 
+		 
+		  
+// on prépare une requete SQL cherchant tous les titres, les dates ainsi que l'auteur des messages pour le membre connecté
+$sql = 'SELECT titre,lu, date,id_expediteur, membre.login as expediteur, messages.id as id_message FROM messages, membre WHERE id_destinataire="'.$_SESSION['id'].'" AND id_expediteur=membre.id AND lu="0" ORDER BY date DESC';
+// lancement de la requete SQL
+$req = mysql_query($sql) or die('Erreur SQL !<br />'.$sql.'<br />'.mysql_error());
+$nb = mysql_num_rows($req);
+
+if ($nb == 0) {
+	echo 'Vous n\'avez aucun nouveau message.';
+}
+else {
+
+
+}
+
+?>
